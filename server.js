d22218a1d0fb75a12fd6e6b4462aca5fc458f7ab
davidlinke <davidlinke@davidlinke.com>
2019-10-14 22:34:09 -0400

    added additional info to primary schedule route

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ db.on('error', err => console.log(err.message));
 db.on('connected', () => console.log('mongo connected'));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
-// IMPORT GTFS DATA
+// Import GTFS Data
 const updateGTFSdata = () => {
 	gtfs
 		.import(config)
@@ -166,6 +166,9 @@ app.get('/schedule/:stop_id/:destination_id', (req, res) => {
 				req.params.destination_id
 			)
 		)
+		.then(trips =>
+			formatStopTimes(trips, req.params.stop_id, req.params.destination_id)
+		)
 		.then(trips => {
 			res.json(trips);
 		});
@@ -228,4 +231,82 @@ const getStopTimesByCurrentStopAndFilter = (trips, stop_id, destination_id) => {
 					});
 			})
 	);
+};
+
+const formatStopTimes = async (data, stop_id, destination_id) => {
+	const getTripStopTime = trip => {
+		return gtfs
+			.getStoptimes({
+				agency_key: 'Metro-North Railroad',
+				trip_id: trip.trip_id,
+				stop_id: destination_id
+			})
+			.then(stopinfo => {
+				// console.log(stopinfo[0].arrival_time);
+				return {
+					arrival_time: stopinfo[0].arrival_time,
+					arrival_timestamp: stopinfo[0].arrival_timestamp
+				};
+			});
+	};
+
+	const getTripInfo = trip_id => {
+		let info = {};
+		return gtfs
+			.getTrips({
+				agency_key: 'Metro-North Railroad',
+				trip_id: trip_id
+			})
+			.then(tripInfo => {
+				info = tripInfo[0];
+				return tripInfo[0];
+			})
+			.then(tripInfo => {
+				return gtfs.getRoutes({
+					agency_key: 'Metro-North Railroad',
+					route_id: tripInfo.route_id
+				});
+			})
+			.then(routeInfo => {
+				return { ...info, ...routeInfo[0] };
+			});
+	};
+
+	const calculateDuration = (start_time, end_time) => {
+		if (start_time < end_time) {
+			return end_time - start_time;
+		} else {
+			return start_time - end_time;
+		}
+	};
+
+	const promises = data.map(async trip => {
+		const stop_times = await getTripStopTime(trip);
+		const trip_info = await getTripInfo(trip.trip_id);
+		return {
+			trip_id: trip.trip_id,
+			trip_headsign: trip_info.trip_headsign,
+			trip_short_name: trip_info.trip_short_name,
+			route_id: trip_info.route_id,
+			route: trip_info.route_long_name,
+			route_color: trip_info.route_color,
+			route_text_color: trip_info.route_text_color,
+			origin_id: trip.stop_id,
+			destination_id: destination_id,
+			departure_time: trip.departure_time,
+			departure_timestamp: trip.departure_timestamp,
+			arrival_time: stop_times.arrival_time,
+			arrival_timestamp: stop_times.arrival_timestamp,
+			trip_duration: calculateDuration(
+				trip.departure_timestamp,
+				stop_times.arrival_timestamp
+			),
+			stop_sequence: trip.stop_sequence,
+			wheelchair_accessible: trip_info.wheelchair_accessible,
+			peak_offpeak: trip_info.peak_offpeak
+		};
+	});
+
+	const formattedData = await Promise.all(promises);
+	return formattedData;
 };
